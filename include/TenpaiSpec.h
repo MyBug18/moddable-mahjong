20fbb16459d9273e600514b7e8f7ce8a0796a637
MyBug18 <mskimkim0318@gmail.com>
2023-05-14 00:12:34 +0900

    added eol in the end of file

---
+++ include/TenpaiSpec.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "HaiCandidates.h"
+#include <unordered_map>
+
+class TenpaiSpec
+{
+private:
+    std::string name;
+    std::unordered_map<std::string, int>* components;
+
+public:
+    const HaiCandidates& GetShangten(const std::vector<Hai*>&, std::unordered_map<std::string, int>*);
+};
