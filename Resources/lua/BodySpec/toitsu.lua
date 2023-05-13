7bd2b898d1b7bf7e450aa1c60a9f7dca10f90c25
MyBug18 <mskimkim0318@gmail.com>
2023-05-13 16:29:46 +0900

    added toitsu

---
+++ Resources/lua/BodySpec/toitsu.lua
@@ -0,0 +1,28 @@
+local m = {}
+
+m.Name = 'toitsu'
+m.BodyType = 'head'
+m.Properties = { 'toitsu' }
+m.ShouldFuro = false
+
+m.GetCandidates = function(hais)
+    local candidates = HaiCandidates.new()
+
+    local l = #hais
+
+    if l ~= 1 then
+        return candidates
+    end
+
+    local hai = hais[1]
+    local haiSpec = hai.HaiSpec
+    local number = haiSpec.Number
+    local haiType = haiSpec.HaiType
+
+    candidates:PushHai(HaiSpec.new(haiType, number))
+    candidates.CompleteCount = 1
+
+    return candidates
+end
+
+return m
